006f627c03221f24e6e41db9fb119127a37161da
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-15 23:26:43 +0300

    almost works

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import data from './json_generator/data';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.colors = [
-            '#b8b8b8',
-            '#00ffb3',
-            '#0084ff',
-            'rgba(127, 0, 255, 0.62, 0.5)'
+        this.linkColors = [
+            'rgba(201, 201, 201, 0.5)',
+            'rgba(0, 255, 179, 0.5)',
+            'rgba(0, 132, 255, 0.5)',
+            'rgba(0, 26, 255, 0.5)'
         ];
         this.widths = [0, 1, 1, 1, 2, 2, 2, 2, 2, 5, 5];
 
@@ -27,22 +27,32 @@ class App extends Component {
           <ForceGraph2D
               ref={el => { this.fg = el; }}
               graphData={data}
-              linkSource={"source"}
-              linkTarget={"target"}
+              nodeLavel={"hello"}
               nodeAutoColorBy="group"
-              nodeThreeObject={node => {
-                  const sprite = new SpriteText(node.id);
-                  sprite.color = node.color;
-                  sprite.textHeight = 1;
-                  return sprite;
+              nodeCanvasObject={(node, ctx, globalScale) => {
+                  const label = node.id;
+                  const fontSize = 4;
+                  ctx.font = `${fontSize}px Sans-Serif`;
+                  const textWidth = ctx.measureText(label).width;
+                      const bckgDimensions = [10, fontSize].map(n => n + fontSize * 0.2); // some padding
+                  // ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+                  // ctx.fillStyle = "#000000";
+                  ctx.beginPath();
+                  ctx.arc(node.x, node.y, 4, 0, 2 * Math.PI);
+                  // ctx.fill();
+                  ctx.fill();
+                  ctx.textAlign = 'center';
+                  ctx.textBaseline = 'middle';
+                  ctx.fillStyle = node.color;
+                  ctx.fillText(label, node.x, node.y);
               }}
-              linkLabel={link => link.value + ', ' + Math.floor(link.value / 3)}
+              linkLabel={link => `${link.source.id} ⟹ ${link.target.id} = ${link.value}`}
               linkWidth={link => this.widths[link.value]}
-              linkColor={link => this.colors[Math.floor(link.value / 3)]}
-              linkDirectionalArrowLength={link => link.value * 0.5}
+              linkColor={link => this.linkColors[Math.floor(link.value / 3)]}
+              linkDirectionalArrowLength={link => this.widths[link.value]}
               linkDirectionalArrowRelPos={0.5}
               // linkDirectionalParticles={3}
-              linkCurvature={0.5}
+              linkCurvature={1}
               cameraPosition={{x: 1000}}
           />
       );
