ef5b653c7b04488509593530859ae57d6cdfa852
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-16 00:19:51 +0300

    split subjects by groups

--- src/App.js
+++ src/App.js
@@ -14,12 +14,23 @@ class App extends Component {
             'rgba(0, 132, 255, 0.5)',
             'rgba(0, 26, 255, 0.5)'
         ];
+        this.groupColors = [
+            '0, 122, 255',
+            '40, 205, 65',
+            '175, 82, 222',
+            '255, 59, 48',
+            '255, 149, 0',
+            '142, 142, 147',
+            '162, 132, 94',
+
+        ];
         this.widths = [0, 1, 1, 1, 2, 2, 2, 2, 2, 5, 5];
+        this.transparencies = [0, .07, .07, .07, .1, .1, .1, .1, .1, .3, .3];
 
     }
 
     componentDidMount() {
-        this.fg.zoom(5);
+        this.fg.zoom(7);
     }
 
     render() {
@@ -27,30 +38,35 @@ class App extends Component {
           <ForceGraph2D
               ref={el => { this.fg = el; }}
               graphData={data}
-              nodeLavel={"hello"}
-              nodeAutoColorBy="group"
+              nodeLabel={"hello"}
+              nodeColor={node => `rgb(${this.groupColors[node.group]})`}
               nodeCanvasObject={(node, ctx, globalScale) => {
-                  const label = node.id;
-                  const fontSize = 4;
+                  const fontSize = 3;
                   ctx.font = `${fontSize}px Sans-Serif`;
-                  const textWidth = ctx.measureText(label).width;
-                      const bckgDimensions = [10, fontSize].map(n => n + fontSize * 0.2); // some padding
-                  // ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-                  // ctx.fillStyle = "#000000";
+                  const textWidth = ctx.measureText(node.id).width;
+                  const bckgDimensions = [10, fontSize].map(n => n + fontSize * .2); // some padding
+                  ctx.fillStyle = `rgb(${this.groupColors[node.group]})`;
+                  ctx.strokeStyle = 'white';
+                  ctx.lineWidth = 0.4;
+
                   ctx.beginPath();
-                  ctx.arc(node.x, node.y, 4, 0, 2 * Math.PI);
-                  // ctx.fill();
+                  ctx.arc(node.x, node.y, 2, 0, 2 * Math.PI);
                   ctx.fill();
-                  ctx.textAlign = 'center';
+                  ctx.stroke();
+
+                  ctx.textAlign = 'left';
                   ctx.textBaseline = 'middle';
                   ctx.fillStyle = node.color;
-                  ctx.fillText(label, node.x, node.y);
+                  ctx.lineWidth = 0.7;
+                  ctx.strokeText(node.id, node.x + 3, node.y);
+                  ctx.fillText(node.id, node.x + 3, node.y);
               }}
               linkLabel={link => `${link.source.id} ⟹ ${link.target.id} = ${link.value}`}
               linkWidth={link => this.widths[link.value]}
-              linkColor={link => this.linkColors[Math.floor(link.value / 3)]}
+              // linkColor={link => this.linkColors[Math.floor(link.value / 3)]}
+              linkColor={link => `rgba(${this.groupColors[link.source.group]}, ${this.transparencies[link.value]})`}
               linkDirectionalArrowLength={link => this.widths[link.value]}
-              linkDirectionalArrowRelPos={0.5}
+              linkDirectionalArrowRelPos={.5}
               // linkDirectionalParticles={3}
               linkCurvature={1}
               cameraPosition={{x: 1000}}
