aae218febbf23db7549ca48e5490a069c25c0078
Alexander Volkov <agvolkov4@yandex.ru>
2019-04-16 00:14:06 +0300

    Updates

---
+++ precache-manifest.d823cbea7705dfd8a387f3589fd37559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ed8220af6da706392ee",
+    "url": "/subjects/static/js/runtime~main.5b66ecc2.js"
+  },
+  {
+    "revision": "bd2b4fd3716c2395b55a",
+    "url": "/subjects/static/js/main.5baaf4e0.chunk.js"
+  },
+  {
+    "revision": "7ef5bbefc1d66ad99feb",
+    "url": "/subjects/static/js/2.71d67553.chunk.js"
+  },
+  {
+    "revision": "bd2b4fd3716c2395b55a",
+    "url": "/subjects/static/css/main.b39a9123.chunk.css"
+  },
+  {
+    "revision": "194ef8150a64cad8d725798b7da8a481",
+    "url": "/subjects/index.html"
+  }
+];
